fd3fb1ede70c344f3e9023cd3a54c29ac69c1d4b
Andrew Enoksen <aenoksen@swatsolutions.com>
2017-11-17 12:51:53 -0600

    finished adding the last error handling.

--- src/test/java/selenium/CustomActions.java
+++ src/test/java/selenium/CustomActions.java
@@ -6,12 +6,17 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class CustomActions extends SeleniumActions{
 
     protected void validateResults(int value){
-        List<WebElement> resultData = webDriver.findElements(By.className("atsSearchResultsData"));
-        int resultsFound = resultData.size()/3;
-        assertTrue(resultsFound == value);
+        try {
+            List<WebElement> resultData = webDriver.findElements(By.className("atsSearchResultsData"));
+            int resultsFound = resultData.size() / 3;
+            assertTrue(resultsFound == value);
+        } catch (Exception e){
+            fail("Error validating the results.");
+        }
     }
 }
